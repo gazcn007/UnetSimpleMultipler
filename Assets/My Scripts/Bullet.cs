da4f0a39399f5c959930840fe56a4c398fdc38f4
Carl <shikang.liu@mail.mcgill.ca>
2017-03-05 02:47:37 -0500

    demo

---
+++ Assets/My Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col){
+		Debug.Log (col.gameObject.name);
+		if (col.gameObject.name == "Player(Clone)"||col.gameObject.name == "Enemy(Clone)") {
+			GameObject hit = col.gameObject;
+			Health health = hit.GetComponent<Health> ();
+			if (health != null) {
+				health.TakeDamage (10);
+			}
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/My Scripts/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class EnemySpawner : NetworkBehaviour {
+
+	public GameObject enemyPrefab;
+	public int numberOfEnemies;
+	public override void OnStartServer(){
+		for (int i = 0; i < numberOfEnemies; i++) {
+			Vector3 spawnPosition = new Vector3 (Random.Range (-8.0f, 8.0f), 0.0f, Random.Range (-9.8f, 8.0f));
+			Quaternion spawnRotatio = Quaternion.Euler (0.0f, Random.Range (0.0f, 180.0f), 0);
+			GameObject enemy = (GameObject)Instantiate (enemyPrefab, spawnPosition, spawnRotatio);
+			NetworkServer.Spawn (enemy);
+		}
+	}
+}
